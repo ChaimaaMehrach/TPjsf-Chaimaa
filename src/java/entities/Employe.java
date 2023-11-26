95302f7ffc5801c78d3d36c35893cc82c059f325
chaimaamehrach <52417699+chaimaa26@users.noreply.github.com>
2023-11-27 00:30:11 +0100

    moment

---
+++ src/java/entities/Employe.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author anasn
+ */
+@Entity
+public class Employe implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+
+    
+    private Date dateNaissance;
+
+    private String Photo;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id") 
+    private Service service;
+
+    @ManyToOne
+    @JoinColumn(name = "chef") 
+    private Employe chef;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(String Photo) {
+        this.Photo = Photo;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public Employe getChef() {
+        return chef;
+    }
+
+    public void setChef(Employe chef) {
+        this.chef = chef;
+    }
+    
+ }
+
+  
+
---
+++ src/java/service/EmployeService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+import entities.Employe;
+/**
+ *
+ * @author amine
+ */
+public class EmployeService extends AbstractFacade<Employe> {
+    
+    
+    @Override
+    protected Class<Employe> getEntityClass() {
+        return Employe.class;
+    }
+}
